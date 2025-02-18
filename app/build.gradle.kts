7349cde943a82116ecb5634fd1113c5b03a14431
Tikky1 <efekankul5161@gmail.com>
2025-02-19 01:14:29 +0300

    Bug Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rolegame.game"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.1"
